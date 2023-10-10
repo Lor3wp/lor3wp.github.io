396fb079ccff6f903a669b82cc5a43412d6db25d
HennaK1 <henna.koli@metropolia.fi>
2023-10-10 13:43:00 +0300

    frontpagecontent ready

--- src/components/GoRentButton.jsx
+++ src/components/GoRentButton.jsx
@@ -1,14 +1,20 @@
 import Button from 'react-bootstrap/Button';
+import PropTypes from 'prop-types';
+import styles from '../css/GorentButton.module.css';
 
 function GoRentButton({ buttonText }) {
   return (
     <>
 
-      <Button className="gotorentbutton" variant="flat" size="l">
+      <Button className={styles.gotorentbutton} variant="flat" size="l">
         {buttonText}
       </Button>
     </>
   );
 }
+
+GoRentButton.propTypes = {
+  buttonText: PropTypes.string.isRequired,
+};
 
 export default GoRentButton;
