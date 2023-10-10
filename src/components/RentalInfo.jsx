e41d6ddadcf7af4f2ce0e108c3d46f35391727b2
HennaK1 <henna.koli@metropolia.fi>
2023-10-10 16:27:15 +0300

    text colors fixed

--- src/components/RentalInfo.jsx
+++ src/components/RentalInfo.jsx
@@ -1,28 +1,30 @@
 import PropTypes from 'prop-types';
+import styles from '../css/FrontPage.module.css';
 
 const RentInfoBoxList = ({ items }) => {
   const highlightText = (text) => {
     const replacements = [
-      { pattern: /5 €/g, replacement: '<span class="highlighted-text">$&</span>' },
-      { pattern: /vain henkilöasiakkaille jätteen kuljettamiseen Sortti-asemille\./g, replacement: '<span class="highlighted-text">$&</span>' },
-      { pattern: /kolmen tunnin/g, replacement: '<span class="highlighted-text">$&</span>' },
-      {pattern: /40 euroa./g, replacement: '<span class="highlighted-text">$&</span>' },
-      { pattern: /Peräkärryä ei voi/g, replacement: '<span class="highlighted-text">$&</span>' },
-      {pattern: /henkilöllisyystodistuksesi/g, replacement: '<span class="highlighted-text">$&</span>' }
+      { pattern: /5 €/g, replacement: `<span class="${styles.highlightedText}">$&</span>` },
+      { pattern: /vain henkilöasiakkaille jätteen kuljettamiseen Sortti-asemille\./g, replacement: `<span class="${styles.highlightedText}">$&</span>` },
+      { pattern: /kolmen tunnin/g, replacement: `<span class="${styles.highlightedText}">$&</span>` },
+      { pattern: /40 euroa\./g, replacement: `<span class="${styles.highlightedText}">$&</span>` },
+      { pattern: /Peräkärryä ei voi/g, replacement: `<span class="${styles.highlightedText}">$&</span>` },
+      { pattern: /henkilöllisyystodistuksesi/g, replacement: `<span class="${styles.highlightedText}">$&</span>` }
     ];
-
+  
     let highlightedText = text;
     replacements.forEach(({ pattern, replacement }) => {
       highlightedText = highlightedText.replace(pattern, replacement);
     });
-
+  
     return highlightedText;
   };
+  
 
   return (
     <ul>
       {items.map((item, index) => (
-        <li className="infolist" key={index} dangerouslySetInnerHTML={{ __html: highlightText(item) }} />
+        <li className={styles.infoList} key={index} dangerouslySetInnerHTML={{ __html: highlightText(item) }} />
       ))}
     </ul>
   );
@@ -33,7 +35,3 @@ RentInfoBoxList.propTypes = {
 };
 
 export default RentInfoBoxList;
-
-
-
-
