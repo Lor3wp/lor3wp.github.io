275776d940b3bb3f580e38d8a75b40af8ae9f688
karoliam <kmultas@gmail.com>
2023-10-12 21:26:50 +0300

    deleted everything from app.jsx for easier merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import UserForm from './components/UserForm';
-import BankButton from './components/BankButton';
-import Payment from './pages/Payment';
+
 
 
 function App() {
   return (
     <>
-<Payment></Payment>
     </>
   );
 }
