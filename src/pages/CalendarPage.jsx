a8e1292290eb10431ae97159c6cc09a748c764b2
HennaK1 <henna.koli@metropolia.fi>
2023-10-13 13:09:21 +0300

    headers merge

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -1,23 +1,25 @@
-import MainButton from "../components/MainButton";
-import { useNavigate } from "react-router-dom";
+import MainButton from '../components/MainButton';
+import { useNavigate } from 'react-router-dom';
+import Header from '../components/Header';
 
 const CalendarPage = () => {
   const navigate = useNavigate();
 
   const handleClick = () => {
-    navigate("/rent-form");
-
-  }
+    navigate('/rent-form');
+  };
   return (
     <>
-    {/* THIS IS JUST MOCKUP CODE PAVEL NEEDS TO FILL IN THIS FILE */}
+      {/* THIS IS JUST MOCKUP CODE PAVEL NEEDS TO FILL IN THIS FILE */}
+      <Header></Header>
       <p>Here will be calendar</p>
       <MainButton
         buttonText="Täytä henkilötiedot"
         type="button"
         size="l"
-        onClick={()=> handleClick()}
-      ></MainButton>    </>
+        onClick={() => handleClick()}
+      ></MainButton>{' '}
+    </>
   );
-}
+};
 export default CalendarPage;
--- src/pages/RentForm.jsx
+++ src/pages/RentForm.jsx
@@ -1,18 +1,19 @@
 import { useState } from 'react';
 import MyCalendar from '../components/Calendar';
 import SelectTime from '../components/SelectTime';
-import '../css/SelectTime.css'
+import '../css/SelectTime.css';
 import SelectProduct from '../components/SelectProduct';
 import '../css/RentForm.css';
 import TimeForm from '../components/TimeForm';
+import Header from '../components/Header';
 
 function RentForm() {
   return (
     <>
+      <Header></Header>
       <TimeForm></TimeForm>
     </>
   );
 }
-
 
 export default RentForm;
