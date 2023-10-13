45521cda2548452ef0cdc387f63dd31f7b6115f9
Lor3wp <61760289+Lor3wp@users.noreply.github.com>
2023-10-13 12:02:52 +0300

    rentform moved to timeform

--- src/pages/RentForm.jsx
+++ src/pages/RentForm.jsx
@@ -4,53 +4,12 @@ import SelectTime from '../components/SelectTime';
 import '../css/SelectTime.css'
 import SelectProduct from '../components/SelectProduct';
 import '../css/RentForm.css';
+import TimeForm from '../components/TimeForm';
 
 function RentForm() {
-  const [selectedDate, setSelectedDate] = useState(new Date());
-  const [selectedTime, setSelectedTime] = useState(null);
-  const currentDate = new Date();
-  const currentMonth = currentDate.getMonth();
-  const currentYear = currentDate.getFullYear();
-  const currentDay = currentDate.getDate();
-  const futureDates = [];
-  for (let i = 1; i < 4; i++) {
-    const randomDate = new Date(currentYear, currentMonth, currentDay + i);
-    futureDates.push(randomDate);
-  }
   return (
     <>
-      <div className="rentBox">
-        <div className="productBox">
-          <SelectProduct></SelectProduct>
-        </div>
-        <hr />
-        <div className="calendarBox">
-          <h2 className="header">Valitse päivä</h2>
-          <MyCalendar
-            futureDates={futureDates}
-            setSelectedDate={setSelectedDate}
-          ></MyCalendar>
-        </div>
-        <hr className="hr" />
-        <div className="selectTimeBox">
-          <SelectTime
-            setSelectedTime={setSelectedTime}
-            stationName={'Kivikko'}
-          ></SelectTime>
-          <SelectTime
-            setSelectedTime={setSelectedTime}
-            stationName={'Konala'}
-          ></SelectTime>
-          <SelectTime
-            setSelectedTime={setSelectedTime}
-            stationName={'Aseman nimi'}
-          ></SelectTime>
-        </div>
-        {/* <p>
-          selecte date: {selectedDate.toLocaleDateString()} <br></br>selected
-          time: {selectedTime}
-        </p> */}
-      </div>
+      <TimeForm></TimeForm>
     </>
   );
 }
