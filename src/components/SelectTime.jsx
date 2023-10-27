0e92363b0cf11c21ed268149393e929efd1f9ab1
Lor3wp <61760289+Lor3wp@users.noreply.github.com>
2023-10-27 13:33:38 +0300

    timeperiodbutton active

--- src/components/SelectTime.jsx
+++ src/components/SelectTime.jsx
@@ -2,13 +2,23 @@ import TimePeriodButton from "./TimePeriodButton";
 import style from '../css/SelectTime.module.css';
 import PropTypes from 'prop-types';
 
-function SelectTime({ setSelectedTime, stationName }) {
+function SelectTime({
+  selectedTime,
+  setSelectedTime,
+  stationName,
+  setSelectedStation,
+  selectedStation,
+}) {
   const timeOptions = ['10-13', '11-14', '12-15', '13-16', '14-17', '15-18'];
   const itemElements = timeOptions.map((item, index) => (
     <TimePeriodButton
       buttonText={item}
       setSelectedTime={setSelectedTime}
       key={index}
+      selectedTime={selectedTime}
+      setSelectedStation={setSelectedStation}
+      stationName={stationName}
+      selectedStation={selectedStation}
     ></TimePeriodButton>
   ));
   return (
@@ -22,8 +32,11 @@ function SelectTime({ setSelectedTime, stationName }) {
 }
 
 SelectTime.propTypes = {
-  setSelectedTime: PropTypes.string.isRequired, // Expect a string prop, and it's required
+  setSelectedTime: PropTypes.func.isRequired, // Expect a string prop, and it's required
+  setSelectedStation: PropTypes.func.isRequired, // Expect a string prop, and it's required
   stationName: PropTypes.string.isRequired, // Expect a string prop, and it's required
+  selectedStation: PropTypes.string.isRequired, // Expect a string prop, and it's required
+  selectedTime: PropTypes.string.isRequired, // Expect a string prop, and it's required
 };
 
 export default SelectTime;
