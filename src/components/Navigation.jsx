fa468f64b432814c4d4e0d464815add13e3122c7
HennaK1 <henna.koli@metropolia.fi>
2023-11-08 11:06:51 +0200

    language dropdown fix to component

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import { useState } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Logo from '../assets/hsy_logo.png';
 import '../css/navigation.css';
@@ -9,17 +10,21 @@ import SwedishFlag from '../assets/sweden-flag-icon.svg';
 
 /* Navigation component */
 const FrontPageNavBar = () => {
-  return (
-    <Navbar bg="white" variant="dark" expand="lg">
-      <Navbar.Brand>
-        <img
-          src={Logo}
-          width="30"
-          height="30"
-          className="logo d-inline-block align-top"
-          alt="Logo"
-        />
-      </Navbar.Brand>
+  const [selectedLanguage, setSelectedLanguage] = useState('Suomi');
+
+  // Language dropdown component
+  const LanguageDropdown = () => {
+    const languageOptions = [
+      { language: 'Suomi', flag: FinnishFlag },
+      { language: 'English', flag: EnglishFlag },
+      { language: 'Svenska', flag: SwedishFlag },
+    ];
+
+    const handleLanguageChange = (language) => {
+      setSelectedLanguage(language);
+    };
+
+    return (
       <div className="dropdown show">
         <a
           className="languageBtn btn btn-secondary dropdown-toggle"
@@ -31,24 +36,44 @@ const FrontPageNavBar = () => {
           aria-expanded="false"
         >
           <i className="bi bi-globe-americas"></i>{' '}
-          <span className="hide-on-small-screens">Suomi</span>
+          <span className="hide-on-small-screens">{selectedLanguage}</span>
         </a>
 
         <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-          <a className="dropdown-item" href="#">
-            <img className="flags" src={FinnishFlag} width="30" height="30" />{' '}
-            Suomi
-          </a>
-          <a className="dropdown-item" href="#">
-            <img className="flags" src={EnglishFlag} width="30" height="30" />
-            English
-          </a>
-          <a className="dropdown-item" href="#">
-            <img className="flags" src={SwedishFlag} width="30" height="30" />
-            Svenska
-          </a>
+          {languageOptions.map((option, index) => (
+            <a
+              className="dropdown-item"
+              href="#"
+              key={index}
+              onClick={() => handleLanguageChange(option.language)}
+            >
+              <img
+                className="flags"
+                src={option.flag}
+                width="30"
+                height="30"
+                alt={option.language}
+              />{' '}
+              {option.language}
+            </a>
+          ))}
         </div>
       </div>
+    );
+  };
+
+  return (
+    <Navbar bg="white" variant="dark" expand="lg">
+      <Navbar.Brand>
+        <img
+          src={Logo}
+          width="30"
+          height="30"
+          className="logo d-inline-block align-top"
+          alt="Logo"
+        />
+      </Navbar.Brand>
+      <LanguageDropdown />
       <div className="container-fluid">
         <div className="container-txt">
           <p>Sorttiasemat</p>
