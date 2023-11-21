238d122d5702f129545b2d6f09a49ee2712086fa
Lor3wp <61760289+Lor3wp@users.noreply.github.com>
2023-11-21 14:37:52 +0200

    config updatet endpoint'

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/HsyTrailer/', // gh pages base config
+  base: '/', // gh pages base config
   plugins: [react()],
 });
 
