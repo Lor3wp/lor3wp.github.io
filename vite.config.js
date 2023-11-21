38e03abc57e1721b7ecb34b0183f716cee267770
Lor3wp <61760289+Lor3wp@users.noreply.github.com>
2023-11-21 14:18:16 +0200

    gitignore updated

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/HsyTrailer/',
+  base: '/HsyTrailer/', // gh pages base config
   plugins: [react()],
 });
 
-// buildpush
