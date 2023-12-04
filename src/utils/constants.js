1af055ecfde337cb8e823008ab75ac0f7d321456
R <ramz9_97@hotmail.com>
2023-12-04 19:04:42 +0200

    stations list UI improvements

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,5 @@
-const API_URL = import.meta.env.VITE_API_URL;
 const NODE_ENV = import.meta.env.NODE_ENV;
 const PUBLIC_DOMAIN = import.meta.env.VITE_PUBLIC_DOMAIN;
 const PUBLIC_PORT = import.meta.env.VITE_PUBLIC_PORT;
 
-export { API_URL, NODE_ENV, PUBLIC_DOMAIN, PUBLIC_PORT };
+export { NODE_ENV, PUBLIC_DOMAIN, PUBLIC_PORT };
